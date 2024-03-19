94b413605c9784f44388c8e65331763d6e9af9cc
stark5200 <a.taha5200@gmail.com>
2024-03-19 04:35:37 +0100

    sign in with google

--- shareme-frontend/src/components/Login.jsx
+++ shareme-frontend/src/components/Login.jsx
@@ -18,6 +18,24 @@ const Login = () => {
           autoPlay
           className='w-full h-full object-cover'
         />
+        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bg-blackOverlay'>
+          <div className='p-5'>
+            <img src={logo} width="130px" alt="logo" />
+          </div>
+          <div className='shadow-2xl'>
+            <GoogleLogin
+              clientId=''
+              render={(renderProbs) => (
+                <button
+                  type="button"
+                  className='bg-mainColor'
+                >
+                  <FcGoogle className='mr-4' /> Sign in with Google
+                </button>
+              )}
+            />
+          </div>
+        </div>
       </div>
     </div>
   )
