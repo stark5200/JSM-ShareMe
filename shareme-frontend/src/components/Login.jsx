fcfed767a2551c659a2954fe3a68dd9f2a704f0d
stark5200 <a.taha5200@gmail.com>
2024-03-19 05:34:34 +0100

    missing response google function

--- shareme-frontend/src/components/Login.jsx
+++ shareme-frontend/src/components/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
           autoPlay
           className='w-full h-full object-cover'
         />
-        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bg-blackOverlay'>
+        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
           <div className='p-5'>
             <img src={logo} width="130px" alt="logo" />
           </div>
@@ -28,11 +28,16 @@ const Login = () => {
               render={(renderProbs) => (
                 <button
                   type="button"
-                  className='bg-mainColor'
+                  className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
                 >
                   <FcGoogle className='mr-4' /> Sign in with Google
                 </button>
               )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy='single_host_origin'
             />
           </div>
         </div>
